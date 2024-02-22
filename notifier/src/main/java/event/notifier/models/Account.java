33443a785e8b9557ec717e0371f95f6ab58e7ef0
VICTORJB <victorjimenezbre@gmail.com>
2024-02-22 15:35:15 -0300

    iniciando....

---
+++ notifier/src/main/java/event/notifier/models/Account.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package event.notifier.models;
+
+/**
+ *
+ * @author Victor J.B
+ */
+public class Account {
+    String login;
+    String senha;
+    String city;
+    String name;
+    
+}
